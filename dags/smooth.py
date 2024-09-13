eae61fa1bef5c2e8169193ae43fa534fe06d881c
Renan Sies <renansies@gmail.com>
2024-09-13 16:17:37 -0300

    running apache airflow

--- dags/smooth.py
+++ dags/smooth.py
@@ -2,14 +2,13 @@ from airflow.decorators import dag
 from airflow.operators.smooth import SmoothOperator
 from datetime import datetime
 
-
 @dag(
     schedule=None,
     start_date=datetime(2023, 1, 1),
     catchup=False,
     tags=["smooth"],
 )
-def smooth()
+def smooth():
     video = SmoothOperator(
         task_id="youtube_video"
     )
